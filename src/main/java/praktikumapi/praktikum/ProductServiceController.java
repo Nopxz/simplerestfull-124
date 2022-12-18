0a7d38629bebb9ca14d53a2b6b9cdf2f2788aa78
Nopxz <naufalps42@gmail.com>
2022-12-18 13:42:03 +0700

    Menambahkan komentar di ProductServiceController

--- src/main/java/praktikumapi/praktikum/ProductServiceController.java
+++ src/main/java/praktikumapi/praktikum/ProductServiceController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author ASUS
  */
+//metode request http get menggunakan hashmap
 @RestController
 public class ProductServiceController {
    private static Map<String, Product> productRepo = new HashMap<>();
@@ -37,19 +38,22 @@ public class ProductServiceController {
       almond.setPrice("Rp. 15000");
       productRepo.put(almond.getId(), almond);
    }
-   
+   //metode untuk menghapus produk
    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
    public ResponseEntity<Object> delete(@PathVariable("id") String id, @RequestBody Product product) { 
       productRepo.remove(id);
       return new ResponseEntity<>("Product is deleted successsfully", HttpStatus.OK);
    }
    
+   //metode untuk mengupdate data
    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
    public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) { 
       
+       //membuat tampilan apabila produk tidak ditemukan
        if(!productRepo.containsKey(id)){
             return new ResponseEntity<>("Product Not Found, Please check again", HttpStatus.NOT_FOUND);
         }
+       //membuat tampilan jika produk berhasil di update atau di edit
         else{
             productRepo.remove(id);
             product.setId(id);
@@ -58,19 +62,20 @@ public class ProductServiceController {
         }
         
     }
-   
+   //metode untuk membuat atau menambah data product
    @RequestMapping(value = "/products", method = RequestMethod.POST)
    public ResponseEntity<Object> createProduct(@RequestBody Product product) {
-        
+        //membuat tampilan apabila id product yang ingin ditambahkan sama atau sudah ada
        if(productRepo.containsKey(product.getId())){
             return new ResponseEntity<>("ID Product Cannot be the Same, please check again", HttpStatus.OK);
         }
+       //membuat tampilan apabila product berhasil ditambahkan atau dibuat
         else{
             productRepo.put(product.getId(), product);
             return new ResponseEntity<>("Product is created Successfully", HttpStatus.CREATED);
         }
     }
-   
+   //metode untuk mendapatkan data product
    @RequestMapping(value = "/products")
    public ResponseEntity<Object> getProduct() {
       return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
